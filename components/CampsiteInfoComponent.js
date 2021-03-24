1c84c07b9d8d346157a32c8c8f7286bb2bb8994e
Greg Noethlich <gmnoethlich@gmail.com>
2021-03-24 17:55:50 -0500

    Secure Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -28,6 +28,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -38,27 +39,30 @@ function RenderCampsite(props) {
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
+              Alert.alert(
+                'Add Favorite',
+                `Are you sure you wish to add ${campsite.name} to favorite?`,
+                [
+                  {
+                    text: 'Cancel',
+                    style: 'cancel',
+                    onPress: () => console.log('Cancel Pressed')
+                  },
+                  {
+                    text: 'OK',
+                    onPress: () => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()
+                  }
+                ],
+                { cancelable: false }
+              )
+            }
+            else if (recognizeComment(gestureState)) {
+              props.onShowModal();
             }
             return true;
-        }
-    });
+          }
+        })
 
     if (campsite) {
         return (
@@ -156,7 +160,6 @@ class CampsiteInfo extends Component {
         this.setState({
             rating: 5,
             author: '',
-            date: '',
             text: ''     
         });
     }
